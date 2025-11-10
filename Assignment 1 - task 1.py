f4e5e91df8c154b99446d9e3047ea20f98f738e9
procoder47 <alok46499@gmail.com>
2025-11-10 22:04:56 +0530

    Add files via upload

---
+++ Assignment 1 - task 1.py
@@ -0,0 +1,8 @@
+num1=10
+num2=20
+print("Enter the first number",num1)
+print("Enter the second number",num2)
+print("Addition:",num1+num2)
+print("Subtraction:",num1-num2)
+print("Division:",num1/num2)
+print("Multiplication:",num1*num2)
---
+++ Assignment 1-task 2.py
@@ -0,0 +1,6 @@
+user1='Mark'
+user2='Hill'
+Greeting='Happy New Year 2025.'
+print("Enter your first name:",user1)
+print("Enter your last name:",user2)
+print('Hello,',user1,user2,'!',Greeting)
